5ba25875d1219fe5a9f552cd6e0636211818c02e
Gurmeet Punia <gurmeetpunia404@gmail.com>
2026-08-12 20:54:23 +0530

    database connection

--- backend/app/database.py
+++ backend/app/database.py
@@ -7,10 +7,11 @@ load_dotenv()
 Database_URL = os.getenv("DATABASE_URL")
 engine = create_engine(
     Database_URL,
-    pool_pre_ping=True,
-    pool_recycle=300,
+    pool_pre_ping=True,   # Har query se pehle connection test karega, agar drop ho chuka hai toh reconnection karega
+    pool_recycle=300,     # Har 5 minute (300 sec) me stale connections drop/refresh karega
+    pool_size=10,         # Maximum open connections
+    max_overflow=20
 )
-
 SessionLocal=sessionmaker(
     autoflush=False,
     autocommit=False,
